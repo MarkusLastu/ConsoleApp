6c5a2ec8c3d05a155e7c46157339c75f0ea31cd0
Marcus Berggren <borkamannen@gmail.com>
2026-08-24 15:08:13 +0200

    MarcusB Första grej

---
+++ MarcusB/MarcusB/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Ange pris per vara: ");
+decimal prisPerVara = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Ange antal: ");
+int antalVaror = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Totalt pris: " + prisPerVara * antalVaror + " kr");
+
