ab9711b4812e0302452716511c9d0b29cef7bc55
Sebbe80 <sebh@live.se>
2026-08-24 15:50:12 +0200

    First

---
+++ Sebbe/Uppgift1.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Ange pris per vara: ");
+
+string prisInput = Console.ReadLine();
+
+decimal pris;
+
+decimal.TryParse(prisInput, out pris);
+
+Console.Write("Ange antal varor: ");
+
+string antalInput = Console.ReadLine();
+
+int antal;
+
+int.TryParse(antalInput, out antal);
+
+decimal totalsumma = pris * antal;
+
+decimal moms = totalsumma * 0.25m;
+
+decimal attBetala = totalsumma + moms;
+
+Console.WriteLine();
+
+Console.WriteLine("Pris per styck: " + pris + " kr");
+
+Console.WriteLine("Antal: " + antal);
+
+Console.WriteLine("Moms (25%): " + moms + " kr");
+
+Console.WriteLine("Att betala: " + attBetala + " kr");
+
