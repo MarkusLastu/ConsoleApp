d0a8f98fd05bde16c344e17f84b9dcc11349ae1c
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-08-24 15:35:53 +0200

    test1

---
+++ NL/test123/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Ange priset för din vara: ");
+string breakfastInput = Console.ReadLine();
+decimal Prisvara = Convert.ToDecimal(breakfastInput);
+Console.WriteLine("Skriv in antal varor: ");
+string quantityInput = Console.ReadLine();
+decimal quantity = Convert.ToDecimal(quantityInput);
+decimal totalprice = (decimal)(Prisvara * quantity * 2);
+Console.WriteLine("Pris efter skatt: " + totalprice);
