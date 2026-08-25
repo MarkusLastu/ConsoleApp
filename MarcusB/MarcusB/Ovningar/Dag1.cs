7d3887af195d171905a7a89182b542f859d93cb0
Marcus Berggren <borkamannen@gmail.com>
2026-08-25 11:41:12 +0200

    Strukturerade lite

--- MarcusB/MarcusB/Ovningar/Dag1.cs
+++ MarcusB/MarcusB/Ovningar/Dag1.cs
@@ -5,6 +5,23 @@ public class Dag1
     public static void Ovn1()
     {
 
+        // Skapa ett Console - program som:
+        // 1.Ber användaren att ange:
+        //  o priset på en vara.
+        //  o hur många varor som köps.
+        // 2.Räknar ut:
+        //  o Totalsumma.
+        //  o moms(25 %)
+        // 3.Skriver ut ett enkelt kvitto
+        // Krav:
+        //  • Använd decimal för pengar.
+        //  • Använd int för antal.
+        // • Använd TryParse.
+        // • Ingen if / else.
+        // • Skriv kommentar om varje rad(ovanför raden).
+
+
+
         int antalVaror = 0;
         decimal prisPerVara = 0;
         decimal pris = 0;
@@ -38,6 +55,20 @@ public class Dag1
 
     public static void Ovn2()
     {
+        // Skapa ett Console - program som:
+        // 1.Ber användaren ange:
+        // • antal timmar
+        // • antal minuter
+        // 2.Konverterar värdena till int.
+        // 3.Räknar ut den totala tiden i minuter.
+        // 4.Skriver ut resultatet.
+        // 5.Skriv kommentar om varje rad(ovanför raden)
+        // Krav:
+        // • Använd int.TryParse.
+        // • Ingen if / else.
+        // • Resultatet ska lagras i en variabel.
+
+
         Console.WriteLine("Övning nummer 2 här!!!");
     }
 }
