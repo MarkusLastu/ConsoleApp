ad48edfac936481516f1e8cd8ff6cab45fc9898e
Marcus Berggren <borkamannen@gmail.com>
2026-08-31 14:11:43 +0200

    v1

--- Grupparbeten_Solution/Grupparbeten_Project/V2_dag1.cs
+++ Grupparbeten_Solution/Grupparbeten_Project/V2_dag1.cs
@@ -8,7 +8,55 @@ public class V2_dag1
 {
     public static void Ovn1_Main()
     {
-        Console.WriteLine("Gandalf");
+        Wizard p1 = new Wizard("Gandalf");
+        Wizard p2 = new Wizard("Börje");
     }
 
 }
+
+public class Wizard(string Name)
+{
+    // Skapa en klass som heter Wizard med Instansdel (hör till varje trollkarl)
+    string Name { get; set; } = Name;
+    int Mana { get; set; } = 100;
+
+
+    // Metoder:
+    public int CastSpell(int cost)
+    {
+        // Minskar trollkarlens Mana
+        Mana = Mana - cost;
+
+        //Returnerar hur mycket mana som användes
+        return CastSpell(cost);
+    }
+
+    public void ReceiveMana(int amount)
+    {
+        //Ökar trollkarlens Mana
+        Mana = Mana + amount;
+    }
+    
+    // Lägg till statisk del i samma klass:
+    // Statisk variabel:
+    public static int TotalSpellsCast(int incomingSpellCount){
+        
+        return incomingSpellCount + 1;
+
+         
+    }
+    // Statisk metod:
+    public static void ShowGlobalStats()
+    {
+        //Skriver ut hur många trollformler som kastats totalt
+        Console.WriteLine("Trollformler som kastats: ");
+    }
+
+
+
+
+
+
+}
+
+
