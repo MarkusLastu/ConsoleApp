09d5b77c746b90a073f1c01fc5c3fd4ab03b591f
Marcus Berggren <borkamannen@gmail.com>
2026-08-31 13:49:40 +0200

    Skapade filer för grupparbeten.

---
+++ Grupparbeten_Solution/Grupparbeten_Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grupparbeten_Project;
+
+Console.WriteLine("Hello, World!");
+
+
+int programVal = 1;
+
+
+if (programVal == 0)
+{
+    Console.WriteLine("Välj program");
+    Console.WriteLine("1 - Gandalf");
+    programVal = int.Parse(Console.ReadLine());
+
+}
+
+
+switch (programVal)
+{
+
+    case 1: V2_dag1.Ovn1_Main(); break;
+}
---
+++ Grupparbeten_Solution/Grupparbeten_Project/V2_dag1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grupparbeten_Project;
+
+public class V2_dag1
+{
+    public static void Ovn1_Main()
+    {
+        Console.WriteLine("Gandalf");
+    }
+
+}
