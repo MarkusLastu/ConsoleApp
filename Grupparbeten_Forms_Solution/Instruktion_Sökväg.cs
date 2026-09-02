bed898b69e27dd2cf0b5861054bc5b9bfab62a0d
Marcus Berggren <borkamannen@gmail.com>
2026-09-02 13:53:20 +0200

    Fixade instruktion på hur man skriver sökväg till ImportFiler

---
+++ Grupparbeten_Forms_Solution/Instruktion_Sökväg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        //Använd denna sökväg för att hitta filer i denna mapp.        
+        string filePath = Path.Combine(
+
+            AppContext.BaseDirectory,
+            @"..\..\..\..\ImportFiler\xxx.txt");
+
+        // BaseDirectory är den mapp där programmet körs ifrån. I Visual Studio är det bin\Debug\net10.0
+        // ..\..\..\..\ tar oss upp fyra nivåer i mappstrukturen, till roten av Solution.
+        // Därifrån kan vi sedan gå ner i ImportFiler-mappen och hitta rätt txt-fil.
+
+        using StreamReader sr = new StreamReader(filePath);
+    }
+}
