246fe39b1420aa11308bdc4a7c239f93a960df94
Marcus Berggren <borkamannen@gmail.com>
2026-09-02 11:02:07 +0200

    Nytt projekt

--- MarcusB_Forms_Solution/MarcusB_Forms_Project/Program.cs
+++ MarcusB_Forms_Solution/MarcusB_Forms_Project/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace MarcusB_Forms_Project
 {
     internal static class Program
@@ -10,6 +12,8 @@ namespace MarcusB_Forms_Project
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Debug.WriteLine("Starting MarcusB_Forms_Project...");
+            Debug.WriteLine("Hello World!");
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
