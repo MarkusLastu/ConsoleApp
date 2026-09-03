2a9e0f643abd1da4cf169e1a31e8562547113943
Markus L <mlastu@live.se>
2026-09-03 13:53:58 +0200

    Beställ knapp med kod till klick funktion

--- Grupparbeten_Solutions/Forms/WPF/V2_Dag4_GrpOvn_Pizza/V2_Dag4_GrpOvn_Pizza/MainWindow.xaml.cs
+++ Grupparbeten_Solutions/Forms/WPF/V2_Dag4_GrpOvn_Pizza/V2_Dag4_GrpOvn_Pizza/MainWindow.xaml.cs
@@ -21,5 +21,48 @@ namespace V2_Dag4_GrpOvn_Pizza
         {
             InitializeComponent();
         }
+
+        private void cmdBestall_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbPizza.SelectedIndex == -1 ||
+               (rbSmall.IsChecked == false && rbMedium.IsChecked == false && rbLarge.IsChecked == false))
+            {
+                MessageBox.Show("Du måste välja både en pizza och en storlek innan du kan beställa!");
+                return;
+            }
+
+            string valdPizza = cmbPizza.SelectedItem.ToString();
+            string valdStorlek = "";
+            int pris = 0;
+
+            if (rbSmall.IsChecked == true)
+            {
+                valdStorlek = "Small";
+                pris = 85;
+                this.Background = Brushes.LightSkyBlue;
+            }
+            else if (rbMedium.IsChecked == true)
+            {
+                valdStorlek = "Medium";
+                pris = 100;
+                this.Background = Brushes.LightGreen;
+            }
+            else if (rbLarge.IsChecked == true)
+            {
+                valdStorlek = "Large";
+                pris = 120;
+                this.Background = Brushes.LightCoral;
+            }
+
+            txtResultat.Text = "Du beställde:\n" +
+                               "Storlek: " + valdStorlek + ".\n" +
+                               "Pizza: " + valdPizza + ".\n\n" +
+                               "Totalpris: " + pris + " kr.";
+
+            cmbPizza.SelectedIndex = -1;
+            rbSmall.IsChecked = false;
+            rbMedium.IsChecked = false;
+            rbLarge.IsChecked = false;
+        }
     }
 }
