394c895d5591607b0fda669d6396981287f98b61
Markus L <mlastu@live.se>
2026-09-03 12:53:30 +0200

    Skapade solution för dagens gruppövning.

---
+++ Grupparbeten_Solutions/Forms/WPF/V2_Dag4_GrpOvn_Pizza/V2_Dag4_GrpOvn_Pizza/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace V2_Dag4_GrpOvn_Pizza
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
