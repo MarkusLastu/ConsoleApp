3df36c00ff8b597ac727eba7036eac5f12a9f723
Marcus Berggren <borkamannen@gmail.com>
2026-09-04 14:32:34 +0200

    Raderade gammal kod

--- Grupparbeten_Solutions/Forms/WPF/V2_Dag5_Fredagsgodis/V2_Dag5_Fredagsgodis/Personer.cs
+++ Grupparbeten_Solutions/Forms/WPF/V2_Dag5_Fredagsgodis/V2_Dag5_Fredagsgodis/Personer.cs
@@ -86,67 +86,6 @@ namespace V2_Dag5_Fredagsgodis
             }
         }
 
-
-
-        public static Gymmare CreateMarkusL()
-        {
-            Gymmare markusL = new Gymmare("Markus L", 500);
-
-            markusL.SuperPower = "Finger Styrka: Trycker HÅRT på datortangenterna";
-            markusL.SpecialAttack = "CapsLock-vrålet: Trycker in CapsLock med en sån enorm kraft att tryckvågen får motståndarens kod att sluta kompilera.";
-            markusL.Specialisering = "Mekanisk Hållfasthet: Kan skriva tusentals rader kod på ett mekaniskt tangentbord utan att fingrarna tar slut på glykogen.";
-            markusL.Weakness = "Merge Conflicts i gymmet: Blir helt handlingsförlamad om någon har lämnat kvar vikter på skivstången utan att städa sin branch först.";
-
-            AddPersonToList(markusL);
-
-            return markusL;
-
-        }
-
-        public static Gamer CreateNiklas()
-        {
-            Gamer niklas = new Gamer("Niklas", "Counter-Strike");
-
-            niklas.SuperPower = "xxx";
-            niklas.SpecialAttack = "yyy";
-            niklas.Specialisering = "zzz";
-            niklas.Weakness = "xxx";
-
-            AddPersonToList(niklas);
-
-            return niklas;
-
-        }
-
-        public static Musiker CreateMarcusB()
-        {
-            Musiker marcusB = new Musiker("Marcus B", "Piano");
-
-            marcusB.SuperPower = "Gehör: Kan spela en låt direkt efter att ha hört den.";
-            marcusB.SpecialAttack = "Fortissimo: Slår an ett ackord med enorm kraft.";
-            marcusB.Specialisering = "Improvisation: Kan skapa musik utan att behöva veta vad som ska spelas i förväg.";
-            marcusB.Weakness = "Kan inte prata och spela samtidigt. Det blir kaos i hjärnan.";
-
-            AddPersonToList(marcusB);
-
-            return marcusB;
-
-        }
-
-        public static Musiker CreateSebbe()
-        {
-            Musiker sebbe = new Musiker("Sebbe", "Luftbastuba");
-
-            sebbe.SuperPower = "xxx";
-            sebbe.SpecialAttack = "yyy";
-            sebbe.Specialisering = "zzz";
-            sebbe.Weakness = "xxx";
-
-            AddPersonToList(sebbe);
-
-            return sebbe;
-        }
-
     }
 
 }
