95c313e5b7455edae120baaf6766956a69e1deda
Markus L <mlastu@live.se>
2026-09-08 14:53:56 +0200

    W3D2 Intro_MVVM

---
+++ MarkusL_Solutions/Forms/WPF/Intro_MVVM/Intro_MVVM/Models/Cat.cs
@@ -0,0 +1,28 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intro_MVVM.Models
+{
+    [Table("cats")]
+    public class Cat : BaseModel
+    {
+        [PrimaryKey("id")]
+        public long Id { get; set; }
+        [Column("shelter_id")]
+        public long ShelterId { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("age")]
+        public int Age { get; set; }
+
+        [Column("color")]
+        public string Color { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
