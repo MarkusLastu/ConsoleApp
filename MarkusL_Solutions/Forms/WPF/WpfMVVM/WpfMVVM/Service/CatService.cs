0eeb92bc104e20cf9237203a3282d2a0ad9f14fa
Markus L <mlastu@live.se>
2026-09-09 09:52:39 +0200

    WPF övning W3D3 MarkusL

---
+++ MarkusL_Solutions/Forms/WPF/WpfMVVM/WpfMVVM/Service/CatService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfMVVM.Models;
+using WpfMVVM.Models;
+
+namespace WpfMVVM.Service
+{
+    public class CatService
+    {
+        private readonly Supabase.Client _client;
+
+        public CatService()
+        {
+            _client = new Supabase.Client(
+                "https://mjmcqdfgsydprdhayxur.supabase.co",
+                "sb_publishable_fj51hH9OZ7HMyfrAkiea7g_Ehq3uWgn");
+        }
+
+        public async Task<List<Cat>> GetCats()
+        {
+            var result = await _client
+                .From<Cat>()
+                .Get();
+            return result.Models;
+        }
+    }
+}
---
+++ MarkusL_Solutions/Forms/WPF/WpfMVVM/WpfMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using WpfMVVM.Models;
+using WpfMVVM.Service;
+using WpfMVVM.Commands;
+
+namespace WpfMVVM.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<Cat> listaMedKatter;
+
+        public List<Cat> Cats
+        {
+            get { return listaMedKatter; }
+            set
+            {
+                listaMedKatter = value;
+
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(Cats)));
+            }
+        }
+
+
+        private Cat valdKatt;
+
+        public Cat SelectedCat
+        {
+            get { return valdKatt; }
+
+            set
+            {
+                valdKatt = value;
+
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(SelectedCat)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private CatService catService;
+        public MainViewModel()
+        {
+            catService = new CatService();
+            LoadCatsCommand = new RelayCommand(() => LoadCats());
+        }
+
+        public async Task LoadCats()
+        {
+            Cats = await catService.GetCats();
+        }
+
+        public ICommand LoadCatsCommand { get; }
+
+    }
+
+}
