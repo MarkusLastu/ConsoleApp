1fdefa4f03c8de39676ef4d58a59904c05c6423a
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-09-09 11:05:53 +0200

    nya saker från min mapp

---
+++ NL/vecka3/WpfMvvmDemo/Models/Cat.cs
@@ -0,0 +1,23 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfMvvmDemo.Models
+{
+
+    [Table("cats")]
+    public class Cat : BaseModel
+    {
+        [PrimaryKey("id")]
+        public long Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("age")]
+        public int Age { get; set; }
+        [Column("color")]
+        public string Color { get; set; }
+    }
+
+}
