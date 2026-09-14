bae7e51bdba827ec6ca6f7b48b784e47abcf1a90
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-09-14 19:54:38 +0200

    japp branch

--- GrupparbeteVecka4/Views/HistoryPage.xaml.cs
+++ GrupparbeteVecka4/Views/HistoryPage.xaml.cs
@@ -6,4 +6,21 @@ public partial class HistoryPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        // Visar en popup med två val och väntar på svar (true / false)
+        bool answer = await DisplayAlert(
+            "Avbryta?",                            // Titel
+            "Är du säker på att du vill gå tillbaka?", // Meddelande
+            "Ja",                                  // Bekräfta (returnerar true)
+            "Nej"                                  // Avbryt (returnerar false)
+        );
+
+        // Om användaren klickade "Ja", navigera bakåt
+        if (answer)
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+        // Om användaren klickade "Nej" händer ingenting och de stannar kvar på sidan
+    }
 }
