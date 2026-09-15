ad8057f916ada13ba566d344bc6346afaf463301
Sebbe80 <sebh@live.se>
2026-09-15 14:20:08 +0200

    sebbes två filer

---
+++ GrupparbeteVecka4/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace GrupparbeteVecka4.Models
+{
+    [Table("players")]
+    public class PlayerModel : BaseModel
+    {
+        [PrimaryKey("player_id", false)]
+        public int PlayerId { get; set; }
+
+        [Column("player_name")]
+        public string PlayerName { get; set; }
+
+        [Column("player_image_url")]
+        public string PlayerImageUrl { get; set; }
+    }
+}
---
+++ GrupparbeteVecka4/Views/UsersProfile.xaml.cs
@@ -0,0 +1,9 @@
+namespace GrupparbeteVecka4.Views;
+
+public partial class Users : ContentPage
+{
+	public Users()
+	{
+		InitializeComponent();
+	}
+}
