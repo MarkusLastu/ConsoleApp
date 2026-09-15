f22c8049ed2fdaa85bf668c67cd17fcc4a494f9c
Marcus Berggren <borkamannen@gmail.com>
2026-09-15 17:05:59 +0200

    Nu kan man välja spelare och antal frågor.

---
+++ GrupparbeteVecka4/ViewModels/QuizPageViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using GrupparbeteVecka4.Commands;
+using GrupparbeteVecka4.Models;
+using GrupparbeteVecka4.Service;
+
+namespace GrupparbeteVecka4.ViewModels
+{
+
+    public class QuizPageViewModel : INotifyPropertyChanged
+    {
+
+        private readonly DBService _service;
+
+        // ------ Hämta personer ------
+        // private Question currentQuestion;
+        // public ICommand AnswerCommand { get; }
+
+        // ------ Hämta personer ------
+        private List<Player> _players = new();
+        public List<Player> Players
+        {
+            get => _players;
+            set
+            {
+                _players = value;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(Players)));
+            }
+        }
+        public Player SelectedPlayer { get; set; }
+
+
+        // ------ Antal frågor ------
+        public List<int> QuestionCounts { get; } = new()
+        {
+            5,
+            10,
+            15,
+            20,
+            25,
+            50
+        };
+
+        private int _numberOfQuestions = 10;
+
+        public int NumberOfQuestions
+        {
+            get => _numberOfQuestions;
+            set
+            {
+                _numberOfQuestions = value;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(NumberOfQuestions)));
+            }
+        }
+
+        // ------ Här är konstruktorn för min ViewModel ------
+        public QuizPageViewModel(DBService service)
+        {
+            _service = service;
+
+            _ = LoadPlayers();
+            //AnswerCommand = new RelayCommand(HandleAnswer);
+        }
+
+        private async Task LoadPlayers()
+        {
+            Debug.WriteLine("Laddar players...");
+            Players = await _service.GetPlayersAsync();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
--- GrupparbeteVecka4/Views/QuizPage.xaml.cs
+++ GrupparbeteVecka4/Views/QuizPage.xaml.cs
@@ -1,11 +1,14 @@
+using GrupparbeteVecka4.ViewModels;
+
 namespace GrupparbeteVecka4.Views;
 
 public partial class QuizPage : ContentPage
 {
-	public QuizPage()
+	public QuizPage(QuizPageViewModel viewModel)
 	{
 		InitializeComponent();
-	}
+        BindingContext = viewModel;
+    }
     private async void OnBackButtonClicked(object sender, EventArgs e)
     {
         // ".." betyder att Shell navigerar bakåt till föregående sida (MainPage)
