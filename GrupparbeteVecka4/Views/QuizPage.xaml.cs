8c5752b1760cd93e4b67ac359a55cb6c287f0907
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-09-16 10:11:36 +0200

    Knappen syns inte innan en spelare har valts

--- GrupparbeteVecka4/Views/QuizPage.xaml.cs
+++ GrupparbeteVecka4/Views/QuizPage.xaml.cs
@@ -14,4 +14,10 @@ public partial class QuizPage : ContentPage
         // ".." betyder att Shell navigerar bakåt till föregående sida (MainPage)
         await Shell.Current.GoToAsync("..");
     }
+    private void OnPlayerPickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        // Visar knappen så fort ett giltigt val har gjorts i Pickern
+        StartGameButton.IsVisible = picker.SelectedIndex != -1;
+    }
 }
