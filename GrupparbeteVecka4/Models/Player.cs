fde471c5cf24ab25cafacd298c858b077afcda6d
Tixalore92 <n.larsson@lexpa26.onmicrosoft.com>
2026-09-17 13:45:55 +0200

    Fixade profilbilder så att de tas från databasen (kan inte lägga till en ny bild just nu men orkar inte göra det)

--- GrupparbeteVecka4/Models/Player.cs
+++ GrupparbeteVecka4/Models/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization; // För JsonIgnore
+using Microsoft.Maui.Controls;
 using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
 
@@ -9,7 +11,7 @@ namespace GrupparbeteVecka4.Models
     [Table("players")]
     public class Player : BaseModel
     {
-        [PrimaryKey("player_id")]   //Andra parametern (false): Är en flagga för om värdet skapas automatiskt av databasen (till exempel en räknare som ökar med 1 för varje ny spelare, s.k. auto-increment eller identity).
+        [PrimaryKey("player_id")]
         public long Id { get; set; }
 
         [Column("player_name")]
@@ -17,5 +19,30 @@ namespace GrupparbeteVecka4.Models
 
         [Column("player_image_url")]
         public string PlayerImageUrl { get; set; }
+
+        // Hjälpegenskap som hanterar både webb-URLer och lokala bildfiler säkert
+        [JsonIgnore]
+        public ImageSource AvatarSource
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(PlayerImageUrl))
+                {
+                    // Kolla om det är en fullständig URL (http:// eller https://)
+                    if (Uri.TryCreate(PlayerImageUrl, UriKind.Absolute, out Uri validUri)
+                        && (validUri.Scheme == Uri.UriSchemeHttp || validUri.Scheme == Uri.UriSchemeHttps))
+                    {
+                        return ImageSource.FromUri(validUri);
+                    }
+
+                    // Om det är ett filnamn/relativ sökväg (t.ex. "avatar.png")
+                    return ImageSource.FromFile(PlayerImageUrl);
+                }
+
+                // Standardbild om bild saknas helt i databasen
+                return ImageSource.FromFile("user_placeholder.png");
+            }
+        }
     }
 }
+    
